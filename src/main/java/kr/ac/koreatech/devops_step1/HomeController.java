073539eed19869302c2d9f3ec43e78e97b23d22e
masungil <masungil70@gmail.com>
2024-05-29 22:18:06 +0900

    index.html 추가

--- src/main/java/kr/ac/koreatech/devops_step1/HomeController.java
+++ src/main/java/kr/ac/koreatech/devops_step1/HomeController.java
@@ -1,13 +1,18 @@
 package kr.ac.koreatech.devops_step1;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 
-@RestController
+
+@Controller
+@Slf4j
 public class HomeController {
     @GetMapping("/")
     public String index() {
-        return "hello world";
+        log.info("hello world");
+        return "index.html";
     }
 }
