127e4f1d6e876266929fc97dd5c7fd92154033ea
masungil <masungil70@gmail.com>
2024-05-29 20:15:42 +0900

    first commit

---
+++ src/test/java/kr/ac/koreatech/devops_step1/DevopsStep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.koreatech.devops_step1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsStep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
